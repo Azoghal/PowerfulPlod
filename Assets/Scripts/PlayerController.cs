cf3b7ad6a1569cbb550860a07caae08895e0efa1
Unknown <sam.beer208@gmail.com>
2018-02-24 10:40:44 +0000

    Original Project
    All the min viable product stuff

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+
+    float speed;
+    public Animator anim;
+    bool attacking;
+    float t;
+    float mouseX;
+    float mouseY;
+    float cd;
+    public bool playing;
+    float stompCD;
+    float explosionRadius;
+    float explosionPower;
+    Camera cam;
+    bool blownUp;
+    CursorLockMode wantedMode;
+
+    // Use this for initialization
+    void Start () {
+        playing = true;
+        explosionRadius = 4;
+        explosionPower = 1000;
+        cd = 2;
+        stompCD = 2;
+        wantedMode = CursorLockMode.Locked;
+        Cursor.lockState = wantedMode;
+        PlayerManager Pm = transform.GetComponent<PlayerManager>();
+        Pm.Setup();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        speed = Input.GetAxis("Vertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        checkAttack();
+        transform.Rotate(new Vector3(0, mouseX, 0));
+        if (attacking == false)
+        {
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+        else if (t > 0.8f && blownUp == false)
+        {
+            blownUp = true;
+            // add explosionscript here
+            Vector3 explosionPos = transform.GetChild(2).position;
+            Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+            foreach (Collider hit in colliders)
+            {
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+                if (rb != null)
+                {
+                    if (hit.CompareTag("World Rigidbody"))
+                    {
+                        rb.AddExplosionForce(explosionPower * 5, explosionPos, explosionRadius);
+
+                    }
+                    else if (rb.gameObject != transform.gameObject && rb.gameObject.CompareTag("Player"))
+                    {
+                        //rb.AddExplosionForce(explosionPower, explosionPos, explosionRadius);
+                        // send force to be recieved
+                        CmdSubmitForce(rb.gameObject, explosionPower, explosionPos, explosionRadius);
+                    }
+                }
+            }
+        }
+
+        anim.SetBool("Attacking", attacking);
+        anim.SetFloat("Speed", speed);
+        cd += Time.deltaTime;
+
+        
+
+    }
+
+    [Command]
+    void CmdSubmitForce(GameObject hit,float explodePower,Vector3 explodePosition, float explodeRadius)
+    {
+        hit.GetComponent<PlayerController>().RpcExecuteKickForce(explodePower,explodePosition,explodeRadius);
+    }
+
+    [ClientRpc]
+
+    void RpcExecuteKickForce(float explodePow, Vector3 explodePosit, float explodeRad)
+    {
+        transform.GetComponent<Rigidbody>().AddExplosionForce(explodePow, explodePosit, explodeRad);
+    }
+
+
+    void checkAttack()
+    {
+        if (Input.GetKeyDown("space") && cd > stompCD)
+        {
+            attacking = true;
+            t = 0;
+            cd = 0;
+            blownUp = false;
+        }
+
+        t+= Time.deltaTime;
+
+        if (t > 1.1f)
+        {
+            attacking = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerManager : NetworkBehaviour {
+
+    [SyncVar]
+    private bool _isDead = false;
+    public bool isDead
+    {
+        get { return _isDead; }
+        protected set { _isDead = value; } 
+    }
+
+    [SerializeField]
+    private Behaviour[] DisableOnDeath;
+    private bool[] WasEnabled;
+    
+    void checkDead()
+    {
+        if (transform.position.y <= -5)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        if (!isDead)
+            Debug.Log("player has died");
+
+        isDead = true;
+
+        //disable components
+
+        for (int i = 0; i < DisableOnDeath.Length; i++)
+        {
+            DisableOnDeath[i].enabled = false;
+        }
+
+        Collider _col = GetComponent<Collider>();
+        if (_col != null)
+            _col.enabled = false;
+
+        Rigidbody _rb = GetComponent<Rigidbody>();
+        if (_rb != null)
+        {
+            _rb.useGravity = false;
+            _rb.velocity = Vector3.zero;
+        }
+           
+
+
+        transform.position = new Vector3(0, 3, -5);
+        transform.rotation = Quaternion.Euler(0,0,0);
+        transform.GetChild(0).GetChild(1).GetComponent<SkinnedMeshRenderer>().enabled = false;
+
+        // call respawn method
+
+        StartCoroutine(respawn());
+
+    }
+
+    private IEnumerator respawn()
+    {
+        yield return new WaitForSeconds(3f);
+        setDefaults();
+        Transform _spawnPoint = NetworkManager.singleton.GetStartPosition();
+        transform.position = _spawnPoint.position;
+
+    }
+
+	// Use this for initialization
+	public void Setup () {
+
+        WasEnabled = new bool[DisableOnDeath.Length];
+        for (int i = 0; i < WasEnabled.Length; i++)
+        {
+            WasEnabled[i] = DisableOnDeath[i].enabled;
+        }
+
+        setDefaults();
+	}
+
+    void setDefaults()
+    {
+        isDead = false;
+
+        for (int i = 0; i < DisableOnDeath.Length; i++)
+        {
+            DisableOnDeath[i].enabled = WasEnabled[i];
+        }
+
+        Collider _col = GetComponent<Collider>();
+        if (_col != null)
+            _col.enabled = true;
+
+        Rigidbody _rb = GetComponent<Rigidbody>();
+        if (_rb != null)
+            _rb.useGravity = true;
+
+        transform.GetChild(0).GetChild(1).GetComponent<SkinnedMeshRenderer>().enabled = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        checkDead();
+	}
+}
